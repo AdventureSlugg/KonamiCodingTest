010377bd75700f59e9320483bfea80986308720b
Zoe <zmb6893@g.rit.edu>
2025-09-20 14:04:27 -0700

    Added in the global colors for the app

---
+++ frontend/styles/colors.ts
@@ -0,0 +1,11 @@
+/**
+ * Contains the golbal color scheme for the app.
+ * @file colors.ts
+ */
+
+// Colors (I used figma's color selector to get the values from the mockup sent in the email)
+export const primaryColor = '#990113';
+export const secondaryColor = '#C2282D';
+export const backgroundColor = '#ffffff';
+export const textColor = '#000000';
+export const placeholderColor = '#B7B7B7';
