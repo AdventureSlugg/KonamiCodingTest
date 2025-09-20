e17f3c63bfd9c2eb8ec67de4b2846f50290c8c13
Zoe <zmb6893@g.rit.edu>
2025-09-20 15:03:20 -0700

    Implemented the formField component

--- frontend/ui/components/form/form.tsx
+++ frontend/ui/components/form/form.tsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Button from "../button/button";
+import FormField, { FormFieldProps } from "./formField";
 
 /**
  * The form component is used to collect user input in a structured manner.
  * @author Zoe Bingham
  */
-
 export default function Form() {
+	const signInFields: Array<FormFieldProps> = [
+		{ type: "text", placeholder: "Username" },
+		{ type: "password", placeholder: "Password" }
+	]
+
 	return (
 		<View>
-			<Text>Form Component</Text>
+			{/** Username and Password Forms */}
+			{
+				// For each field in the signInFields array, create a FormField component
+				signInFields.map((field, index) => (
+					<FormField key={index} {...field}></FormField>
+				))
+			}
+
+			{/** Submission Button */}
+			<Button></Button>
 		</View>
 	)
 }
--- frontend/ui/components/form/formField.tsx
+++ frontend/ui/components/form/formField.tsx
@@ -1,15 +1,47 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
+import { PLACEHOLDER_COLOR } from "../../../styles/colors";
+import { useSizeScheme } from "../../../hooks/use-size-scheme";
+
+export type FormFieldProps = {
+	type: "text" | "password";
+	placeholder: string;
+	// TODO add validator...
+}
 
 /**
  * The FormField component represents a single field within a form.
  * @author Zoe Bingham
  */
+export default function FormField(field: FormFieldProps) {
+	const sizes = useSizeScheme();
+	const [value, setValue] = React.useState("");
 
-export default function FormField() {
 	return (
 		<View>
-			<Text>Form Field Component</Text>
+			<TextInput
+				placeholder={field.placeholder}
+				secureTextEntry={field.type === "password"}
+				style={{
+					...styles.input,
+					fontSize: sizes.fontSize,
+					borderRadius: 5,
+					padding: sizes.padding * .5,
+					paddingLeft: sizes.padding,
+					marginVertical: sizes.padding * .5,
+					color: value ? 'black' : PLACEHOLDER_COLOR,
+				}}
+				onChangeText={setValue}
+			/>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	input: {
+		borderColor: PLACEHOLDER_COLOR,
+		color: PLACEHOLDER_COLOR,
+		borderWidth: 1,
+		backgroundColor: 'white',
+	}
+})
